f9ad33107b37a9e9c05bb49d454ef8c1681c896a
Nicole Almeida <161654593+NicoleIdav@users.noreply.github.com>
2024-05-21 15:30:43 -0300

    Add files via upload

---
+++ main/java/com/web/niconversor2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.web.niconversor2
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.web.niconversor2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonClicar.setOnClickListener {
+            val dolares = binding.editValor.text.toString().trim()
+            val taxa = binding.editTaxa.text.toString().trim()
+
+            if (!dolares.isEmpty() && !taxa.isEmpty()) {
+                val reais = String.format("%.2f", dolares.toDouble() * taxa.toDouble())
+                binding.textResultado.text = "R$" + reais
+            } else {
+                Toast.makeText(applicationContext, "Errou!!!!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
